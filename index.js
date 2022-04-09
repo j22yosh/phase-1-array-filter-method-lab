cc07b37230c935b06ec55610f1cb93960dbe3d90
Joshua <j22yosh@gmail.com>
2022-04-09 11:58:42 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution here
+
+
+
+function findMatching(array, string){
+const filtered = array.filter(function(driver){
+    return (driver.toLowerCase() ===string.toLowerCase())
+   
+
+}
+)
+return filtered;}
+
+
+
+//     for (let user in array)
+//     if (array.toLowerCase() === string.toLowerCase()){
+//         return true
+//     }
+//     else {return false}
+//     return filtered;
+    
+    
+// })}
+
+    function fuzzyMatch(drivers, string){
+        const match = drivers.filter(function(drivers){
+
+            if( string === drivers.slice(0,2)){
+                return drivers
+            }
+            else return drivers === string
+            })
+            return match
+    }
+
+    function matchName (drivers, string){
+        const match = drivers.filter(function(string){
+            for (let user in drivers){
+                if (string === drivers[0]){
+                    return drivers
+                }
+                else return string === drivers[4]
+            }
+        })
+        return match
+    }
